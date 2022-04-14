72ff9a7cdca236a86d8a1bfbb8e8b60a1f146b44
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-14 15:47:51 +0700

    Section 5 and footer

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Section1 from './Sections/Section1';
 import Section2 from './Sections/Section2';
 import Section3 from './Sections/Section3';
 import Section4 from './Sections/Section4';
+import Section5 from './Sections/Section5';
+import Footer from './Sections/Footer';
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
+      <Footer />
     </main>
   );
 };
