a33f715d042805503c186f2c50b8728009f41b3f
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-14 14:03:27 +0700

    Home page section 2 and button

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from '../../navbar/Navbar';
 import Section1 from './Sections/Section1';
+import Section2 from './Sections/Section2';
+
 const Home = () => {
   return (
     <main>
       <Section1 />
+      <Section2 />
     </main>
   );
 };
---
+++ src/components/pages/home/Sections/Section3.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Section3.module.scss';
+import { ArrowButton } from '../../../buttons/Buttons';
+
+const Section3 = () => {
+  return <div>Section3</div>;
+};
+
+export default Section3;
