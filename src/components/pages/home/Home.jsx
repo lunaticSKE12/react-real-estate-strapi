7ba5af7133b9234964047b3cda657a01f9d07b4b
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-14 13:19:42 +0700

    Home page section 1 and assets

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Navbar from '../../navbar/Navbar';
-
+import Section1 from './Sections/Section1';
 const Home = () => {
   return (
     <main>
-      <Navbar />
+      <Section1 />
     </main>
   );
 };
