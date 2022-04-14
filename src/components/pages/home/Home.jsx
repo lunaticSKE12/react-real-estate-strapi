9d072bfef0f9efb41bea0690a844cdfc3992e692
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-04-14 14:50:02 +0700

    Section 3 houses card and info

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from '../../navbar/Navbar';
 import Section1 from './Sections/Section1';
 import Section2 from './Sections/Section2';
+import Section3 from './Sections/Section3';
 
 const Home = () => {
   return (
     <main>
       <Section1 />
       <Section2 />
+      <Section3 />
     </main>
   );
 };
--- src/components/pages/home/Sections/Section3.jsx
+++ src/components/pages/home/Sections/Section3.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 import styles from './Section3.module.scss';
 import { ArrowButton } from '../../../buttons/Buttons';
+import Carousel from '../../../Carousel/Carousel';
+import { SwiperSlide } from 'swiper/react';
+import Card from '../../../Card/Card';
 
 const Section3 = () => {
-  return <div>Section3</div>;
+  return (
+    <section className={styles.section_3}>
+      <div className={styles.section_3_title}>
+        <h1>Best Houses</h1>
+        <ArrowButton text="See more" path="buy" />
+      </div>
+
+      {/* CARDS / CAROUSEL */}
+      <div className={styles.cards}>
+        <Carousel>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Card />
+          </SwiperSlide>
+        </Carousel>
+      </div>
+    </section>
+  );
 };
 
 export default Section3;
